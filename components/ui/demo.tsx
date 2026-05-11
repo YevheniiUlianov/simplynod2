c8f41c4d14690e0f92bad4030da999dbbcb82e85
Yevhenii Ulianov <k2@oldmin.org>
2026-05-11 20:23:49 +0200

    fix: use HeroShapes instead of HeroGeometric

--- components/ui/demo.tsx
+++ components/ui/demo.tsx
@@ -1,7 +1,7 @@
-import { HeroGeometric } from "@/components/ui/shape-landing-hero";
+import { HeroShapes } from "@/components/ui/shape-landing-hero";
 
 function DemoHeroGeometric() {
-  return <HeroGeometric badge="Kokonut UI" title1="Elevate Your" title2="Digital Vision" />;
+  return <HeroShapes badge="Kokonut UI" title1="Elevate Your" title2="Digital Vision" />;
 }
 
 export { DemoHeroGeometric };
